45fd3001d0e66d4b8a4cf4d5a726cf210fd70d4e
omerfarukbakir <omerfarukbakir@gmail.com>
2023-02-27 14:20:52 +0300

    Template Engine & Middilewares

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require("express")
+const path = require("path")
+const ejs = require("ejs")
+
 const app = express()
 const port = 3000
 
+app.set("view engine", "ejs")
+
+app.use(express.static('public'))
+
 app.get("/", (req, res) => {
-
-    const blog = {
-        id: 1,
-        title: "Blog title",
-        description: "Blog description"
-    }
-    res.send(blog)
+    res.render("index")
+})
+app.get("/about", (req, res) => {
+    res.render("about")
+})
+app.get("/add_post", (req, res) => {
+    res.render("add_post")
+})
+app.get("/post", (req, res) => {
+    res.render("post")
 })
 
-app.listen(port,()=>{
-    console.log(`Sunucu ${port} portunda başlatıldı..`)
-})
+app.listen(port)
