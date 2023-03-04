d9ac9cfdb4e02aa00c579ebaa14ff3a4d77afedc
omerfarukbakir <omerfarukbakir@gmail.com>
2023-03-04 15:49:26 +0300

    Modal and Dynamic Content

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const path = require("path")
 const ejs = require("ejs")
+const Post = require("./models/Post")
 
 const app = express()
 const port = 3000
 
+mongoose.connect("mongodb://localhost/cleanblog-test-db")
+
 app.set("view engine", "ejs")
 
 app.use(express.static('public'))
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.render("index")
+app.get("/", async (req, res) => {
+    const posts = await Post.find({})
+    res.render("index",{
+        posts
+    })
 })
+
 app.get("/about", (req, res) => {
     res.render("about")
 })
@@ -20,6 +32,11 @@ app.get("/add_post", (req, res) => {
 })
 app.get("/post", (req, res) => {
     res.render("post")
+})
+
+app.post("/posts", async (req, res) => {
+    await Post.create(req.body)
+    res.redirect("/")
 })
 
 app.listen(port)
